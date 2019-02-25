d0f4214818e1d5848168f0d70e597abb27456f12
wampa842 <wampa842@gmail.com>
2019-02-25 02:53:54 +0100

    added stuff

---
+++ Assets/Scripts/TransformControlBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace MUPS.UI
+{
+    class TransformControlBehaviour : MonoBehaviour, IDragHandler
+    {
+        public void Transform(int type)
+        {
+            Debug.Log("t");
+        }
+    }
+}
